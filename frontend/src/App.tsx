8e90d190615b65bc914a3780e108ad6c55074814
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-06 19:20:37 -0300

    feat: create frontend initial structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>Olá, Conexa!</h1>
+      <p>
+        Easter Egg do primeiro commit:
+        estou me esforçando 110% para esta vaga ser minha!
+      </p>
+    </>
+  );
+}
+
+export default App;
