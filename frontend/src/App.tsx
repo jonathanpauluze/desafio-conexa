0a530de3c85a9b5d5d908a8250e81613b5811ae9
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-06 20:08:36 -0300

    feat: create global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { GlobalStyle } from './styles/global';
 import Routes from './routes';
 
 const App: FC = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes />
     </BrowserRouter>
   );
