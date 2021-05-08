8f469d543deb1bf4bba6c99cca1548ee589d6afc
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-08 15:42:22 -0300

    feat: redirect to login if user's not authenticated

--- frontend/src/pages/Appointments/index.tsx
+++ frontend/src/pages/Appointments/index.tsx
@@ -1,4 +1,7 @@
 import { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Header } from '../../components/Header';
 import { AppointmentsList } from '../../components/AppointmentsList';
@@ -7,6 +10,13 @@ import { FixedFooter } from '../../components/FixedFooter';
 import { AppointmentsContainer } from './styles';
 
 const Appointments: FC = () => {
+  const { token } = useAuth();
+  const history = useHistory();
+
+  if (!token) {
+    history.push('/');
+  }
+
   return (
     <>
       <Header />
