d7cb9a3da1745b00a9edabce6773a9e2259bcd37
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-08 00:46:09 -0300

    feat: add AppointmentsList & FixedFooter in page

--- frontend/src/pages/Appointments/index.tsx
+++ frontend/src/pages/Appointments/index.tsx
@@ -1,23 +1,23 @@
 import { FC } from 'react';
 
 import { Header } from '../../components/Header';
-import { EmptyState } from '../../components/EmptyState';
+import { AppointmentsList } from '../../components/AppointmentsList';
+import { FixedFooter } from '../../components/FixedFooter';
 
 import { AppointmentsContainer } from './styles';
 
 const Appointments: FC = () => {
-  const appointments: string[] = [];
-
   return (
     <>
       <Header />
+
       <AppointmentsContainer>
         <h1>Consultas</h1>
 
-        {!appointments.length ? (
-          <EmptyState>Não há nenhuma consulta agendada</EmptyState>
-        ) : null}
+        <AppointmentsList />
       </AppointmentsContainer>
+
+      <FixedFooter />
     </>
   );
 };
