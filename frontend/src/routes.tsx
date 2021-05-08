5a24478dbb5902fa49ffe3c2fbc815c49f67fa2c
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-07 22:18:39 -0300

    feat: add appointments route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { SignIn } from './pages/SignIn';
+import { Appointments } from './pages/Appointments';
 
 const Routes: FC = () => {
   return (
     <Switch>
       <Route path="/" component={SignIn} exact />
+      <Route path="/appointments" component={Appointments} />
     </Switch>
   );
 };
