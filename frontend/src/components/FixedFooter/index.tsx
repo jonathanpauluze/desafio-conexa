1d19c4a491480a47b46936b306fc95f842eeb77a
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-09 14:39:03 -0300

    feat: create HelpModal component

--- frontend/src/components/FixedFooter/index.tsx
+++ frontend/src/components/FixedFooter/index.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useState, useCallback } from 'react';
 
 import { ButtonOutline } from '../ButtonOutline';
 import { Button } from '../Button';
+import { HelpModal } from './HelpModal';
 
 import { FixedFooterContainer } from './styles';
 
@@ -10,15 +11,34 @@ interface FixedFooterProps {
 }
 
 const FixedFooter: FC<FixedFooterProps> = ({ onOpenNewTransactionModal }) => {
+  const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
+
+  const handleOpenHelpModal = useCallback(() => {
+    setIsHelpModalOpen(true);
+  }, []);
+
+  const handleCloseHelpModal = useCallback(() => {
+    setIsHelpModalOpen(false);
+  }, []);
+
   return (
-    <FixedFooterContainer>
-      <div>
-        <ButtonOutline type="button">Ajuda</ButtonOutline>
-        <Button type="button" onClick={onOpenNewTransactionModal}>
-          Agendar Consulta
-        </Button>
-      </div>
-    </FixedFooterContainer>
+    <>
+      <FixedFooterContainer>
+        <div>
+          <ButtonOutline type="button" onClick={handleOpenHelpModal}>
+            Ajuda
+          </ButtonOutline>
+          <Button type="button" onClick={onOpenNewTransactionModal}>
+            Agendar Consulta
+          </Button>
+        </div>
+      </FixedFooterContainer>
+
+      <HelpModal
+        isOpen={isHelpModalOpen}
+        onRequestClose={handleCloseHelpModal}
+      />
+    </>
   );
 };
 
