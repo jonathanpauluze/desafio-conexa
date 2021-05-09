e5835e0bcf4f1a4f5557081492265075cc918dbc
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-05-08 22:59:47 -0300

    fix: add spacing to show content behind fixed footer

--- frontend/src/pages/Appointments/styles.ts
+++ frontend/src/pages/Appointments/styles.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const AppointmentsContainer = styled.main`
   width: 100%;
   max-width: 1366px;
-  margin: 0 auto;
+  margin: 2rem auto 4.5rem;
   padding: 0 1rem;
 
   h1 {
-    margin: 2rem 0 2.5rem 1rem;
+    margin: 0 0 2.5rem 1rem;
     font-size: 3rem;
     font-weight: bold;
     color: var(--blue-dark);
